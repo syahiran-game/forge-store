579c91eba950ad919bf61899b7584d5ba0d40291
syahiran-game <sazuanmukris78@gmail.com>
2026-05-31 20:37:58 +0800

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = 'https://lwtwefrubpupkkzcmhsa.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3dHdlZnJ1YnB1cGtremNtaHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMDIxMzAsImV4cCI6MjA5NTc3ODEzMH0.cnL955J1xzo1491FAvHh_g4eUqxzSoQPbVlGiy9Dm60'
 
 const TELEGRAM_BOT_TOKEN = '8808527077:AAGVasdsCtorA9n6Nrh5uNjVRguYMGaRkzk'
-const TELEGRAM_CHAT_ID = '8333284745'
+const TELEGRAM_CHAT_ID = '-1003790719872'
 
 window.addEventListener('load', () => {
   if (typeof window.supabase === 'undefined') {
